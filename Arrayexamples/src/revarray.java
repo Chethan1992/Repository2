3584ffce12a619d0de1c1bf11edca1a9fac95c15
chethan1992 <chethannb92@gmail.com>
2017-05-23 13:13:13 +0530

    second

---
+++ Arrayexamples/src/revarray.java
@@ -0,0 +1,25 @@
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author admin
+ */
+public class revarray {
+     public static void main(String[] args) {
+
+ String str="MY name is chethan";
+ //String reverse=new StringBuilder(str).reverse().toString();
+                //OR
+ String reverse=new StringBuffer(str).reverse().toString();
+         System.out.println("normal string is  :"+str+ " \n reverse string  :"+reverse);
+         
+     }
+}
